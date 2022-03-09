d2d4bf1027f384bc88bb88f4c00510bb0560b942
Jude Quintana <jude.quintana@jumpcloud.com>
2022-03-09 12:58:43 -0700

    generate

--- networking_trifecta_demo/networking.tf
+++ networking_trifecta_demo/networking.tf
@@ -76,7 +76,7 @@ module "intra_vpc_security_group_rules" {
 
 # This TGW Centralized router module will attach all vpcs (attachment for each AZ) to one TGW
 # associate and propagate to a single route table
-# add routes in each VPC to all other networks.
+# generate and add routes in each VPC to all other networks.
 module "tgw_centralized_router" {
   source = "git@github.com:JudeQuintana/terraform-modules.git//networking/transit_gateway_centralized_router_for_tiered_vpc_ng?ref=v1.2.2"
 
