15f0f6e860893e5a3d1244f088b43afb3b67aa3c
Jude Quintana <jude.quintana@gmail.com>
2022-06-19 22:25:54 -0600

    updates

--- super_router_experiment/networking.tf
+++ super_router_experiment/networking.tf
@@ -25,7 +25,22 @@ module "tgw_centralized_router_usw2" {
   amazon_side_asn  = 64520
   vpcs = {
     cicd = {
+      az_to_private_route_table_id = {
+        a = "rtb-0f8deb7a6682793e2"
+      }
+      az_to_public_route_table_id = {
+        a = "rtb-09a4481eb3684abba"
+      }
       network = "10.0.0.0/20"
+    }
+    other = {
+      az_to_private_route_table_id = {
+        a = "rtb-108deb7a668271111"
+      }
+      az_to_public_route_table_id = {
+        a = "rtb-10a4481eb36842222"
+      }
+      network = "192.168.16.0/20"
     }
   }
 }
@@ -42,7 +57,24 @@ module "tgw_centralized_router_use1" {
   amazon_side_asn  = 64519
   vpcs = {
     app = {
+      az_to_private_route_table_id = {
+        a = "rtb-0468efad92cd62ab8"
+        b = "rtb-02ad79df1a7c192e7"
+      }
+      az_to_public_route_table_id = {
+        a = "rtb-06b216fb818494594"
+        b = "rtb-06b216fb818494594"
+      }
       network = "172.16.0.0/20"
+    }
+    general = {
+      az_to_private_route_table_id = {
+        c = "rtb-01e5ec4882154a9a1"
+      }
+      az_to_public_route_table_id = {
+        c = "rtb-0ad6cde89a9e386fd"
+      }
+      network = "192.168.0.0/20"
     }
   }
 }
@@ -61,3 +93,12 @@ module "tgw_super_router_usw2" {
   local_centralized_routers = [module.tgw_centralized_router_usw2] # local list must be all same region as each other in aws.local provider. maybe this should be a map instead?
   peer_centralized_routers  = [module.tgw_centralized_router_use1] # peer list must all be same region as each other in aws.peer provider. maybe this should be a map instead?
 }
+
+output "tgw_super_router_generated_local_vpc_routes" {
+  value = module.tgw_super_router_usw2.generated_local_vpc_routes
+}
+
+output "tgw_super_router_generated_peer_vpc_routes" {
+  value = module.tgw_super_router_usw2.generated_peer_vpc_routes
+}
+
