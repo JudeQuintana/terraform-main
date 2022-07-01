4891a8473102c5405764560ed23ba1f1aa48e355
Jude Quintana <jude.quintana@gmail.com>
2022-07-01 12:34:58 -0600

    clean up

--- super_router_experiment/networking.tf
+++ super_router_experiment/networking.tf
@@ -103,26 +103,6 @@ module "tgw_centralized_router_usw2" {
   region_az_labels = var.region_az_labels
   amazon_side_asn  = 64520
   vpcs             = module.vpcs_usw2
-  #vpcs                         = {
-  #cicd                         = {
-  #az_to_private_route_table_id = {
-  #a                            = "rtb-0f8deb7a6682793e2"
-  #}
-  #az_to_public_route_table_id = {
-  #a = "rtb-09a4481eb3684abba"
-  #}
-  #network = "10.0.0.0/20"
-  #}
-  #other = {
-  #az_to_private_route_table_id = {
-  #a = "rtb-108deb7a668271111"
-  #}
-  #az_to_public_route_table_id = {
-  #a = "rtb-10a4481eb36842222"
-  #}
-  #network = "192.168.16.0/20"
-  #}
-  #}
 }
 
 locals {
@@ -209,28 +189,6 @@ module "tgw_centralized_router_use1" {
   region_az_labels = var.region_az_labels
   amazon_side_asn  = 64519
   vpcs             = module.vpcs_use1
-  #vpcs                         = {
-  #app                          = {
-  #az_to_private_route_table_id = {
-  #a                            = "rtb-0468efad92cd62ab8"
-  #b                            = "rtb-02ad79df1a7c192e7"
-  #}
-  #az_to_public_route_table_id = {
-  #a = "rtb-06b216fb818494594"
-  #b = "rtb-06b216fb818494594"
-  #}
-  #network = "172.16.0.0/20"
-  #}
-  #general = {
-  #az_to_private_route_table_id = {
-  #c = "rtb-01e5ec4882154a9a1"
-  #}
-  #az_to_public_route_table_id = {
-  #c = "rtb-0ad6cde89a9e386fd"
-  #}
-  #network = "192.168.0.0/20"
-  #}
-  #}
 }
 
 module "tgw_super_router_usw2" {
