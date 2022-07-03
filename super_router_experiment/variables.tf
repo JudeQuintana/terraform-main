c43a4d064a9ce56f71fe641e37d2cc8a755e4af5
Jude Quintana <jude.quintana@gmail.com>
2022-07-03 10:06:18 -0600

    not needed

--- super_router_experiment/variables.tf
+++ super_router_experiment/variables.tf
@@ -8,37 +8,9 @@ variable "base_region" {
   default     = "us-west-2"
 }
 
-variable "base_ec2_instance_attributes_usw2" {
-  description = "base attributes for building in us-west-2"
-  type = object({
-    ami           = string
-    key_name      = string
-    instance_type = string
-  })
-  default = {
-    key_name      = "tnt-demo-usw2"         # EC2 key pair name to use when launching an instance
-    ami           = "ami-0518bb0e75d3619ca" # AWS Linux 2 us-west-2
-    instance_type = "t2.micro"
-  }
-}
-
 variable "cross_region" {
   description = "cross region/same acct"
   default     = "us-east-1"
-}
-
-variable "base_ec2_instance_attributes_use1" {
-  description = "base attributes for building in us-west-2"
-  type = object({
-    ami           = string
-    key_name      = string
-    instance_type = string
-  })
-  default = {
-    key_name      = "my-ec2-key-use1"       # EC2 key pair name to use when launching an instance
-    ami           = "ami-0742b4e673072066f" # AWS Linux 2 us-east-1
-    instance_type = "t2.micro"
-  }
 }
 
 variable "region_az_labels" {
