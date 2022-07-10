4e417886631dc0cb597e05e84f9da52dc8a5dac9
Jude Quintana <jude.quintana@gmail.com>
2022-07-10 16:07:08 -0600

    same region routing works now

--- super_router_experiment/networking.tf
+++ super_router_experiment/networking.tf
@@ -121,7 +121,12 @@ module "tgw_centralized_router_use1" {
   vpcs             = module.vpcs_use1
 }
 
-# peering between the super router and centralized_routers within the same region and cross region works now (within same account).
+# peering and routing between the super router and centralized_routers within the same region and cross region works now (within same aws account only).
+# The caveat is the peer TGWs will have to go through the local provider region to get to other peer TGWs.
+#
+# ie usw2 vpc 1 <-> usw2 centralized router 1 <-> usw2 super router <-> usw2 centralized router 2 <-> usw2 vpc 2
+#
+# ie use1 vpc 1 <-> use1 centralized router 1 <-> usw2 super router <-> use1 centralized router 2 <-> use1 vpc 2
 module "tgw_super_router_usw2" {
   source = "git@github.com:JudeQuintana/terraform-modules.git//networking/tgw_super_router_for_tgw_centralized_router?ref=tgw-super-router-prep"
 
@@ -133,6 +138,10 @@ module "tgw_super_router_usw2" {
   env_prefix                = var.env_prefix
   region_az_labels          = var.region_az_labels
   local_amazon_side_asn     = 64521
-  local_centralized_routers = [module.tgw_centralized_router_usw2] # local list must be all same region as each other in aws.local provider but cant add to this list just yet.
-  peer_centralized_routers  = [module.tgw_centralized_router_use1] # peer list must all be same region as each other in aws.peer provider. but cant add to this list just yet.
+  local_centralized_routers = [module.tgw_centralized_router_usw2] # local list must be all same region as each other in aws.local provider.
+  peer_centralized_routers  = [module.tgw_centralized_router_use1] # peer list must all be same region as each other in aws.peer provider.
+
+  # You can now add to the list above
+  #local_centralized_routers = [module.tgw_centralized_router_usw2, module.tgw_centralized_router_usw2_another]
+  #peer_centralized_routers  = [module.tgw_centralized_router_use1, module.tgw_centralized_router_use1_another]
 }
