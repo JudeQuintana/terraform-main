b96b67db605ea0be9eb70ffd85908f851078a4d9
Jude Quintana <jude.quintana@gmail.com>
2022-10-18 11:42:06 -0600

    sg rules in separate file

--- networking_trifecta_demo/networking.tf
+++ networking_trifecta_demo/networking.tf
@@ -44,36 +44,6 @@ module "vpcs" {
   tier             = each.value
 }
 
-# This will create a sg rule for each vpc allowing inbound-only ports from
-# all other vpc networks (excluding itself)
-# Basically allowing ssh and ping communication across all VPCs.
-locals {
-  intra_vpc_security_group_rules = [
-    {
-      label     = "ssh"
-      from_port = 22
-      to_port   = 22
-      protocol  = "tcp"
-    },
-    {
-      label     = "ping"
-      from_port = 8
-      to_port   = 0
-      protocol  = "icmp"
-    }
-  ]
-}
-
-module "intra_vpc_security_group_rules" {
-  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/intra_vpc_security_group_rule_for_tiered_vpc_ng?ref=v1.4.5"
-
-  for_each = { for r in local.intra_vpc_security_group_rules : r.label => r }
-
-  env_prefix = var.env_prefix
-  vpcs       = module.vpcs
-  rule       = each.value
-}
-
 # This TGW Centralized router module will attach all vpcs (attachment for each AZ) to one TGW
 # associate and propagate to a single route table
 # generate and add routes in each VPC to all other networks.
