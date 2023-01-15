41f835d45ed5f6e0d734d384616e477fb5da7058
Jude Quintana <jude.quintana@gmail.com>
2023-01-15 11:07:22 -0700

    fix comment

--- super_router_demo/intra_vpc_access.tf
+++ super_router_demo/intra_vpc_access.tf
@@ -38,7 +38,6 @@
 #}
 #}
 
-## make this module
 ## need precondition validation for region
 #module "intra_vpc_access_use1" {
 #source = "git@github.com:JudeQuintana/terraform-modules.git//networking/intra_vpc_security_group_rule_for_tiered_vpc_ng?ref=moar-better"
@@ -56,6 +55,7 @@
 #}
 #}
 
+## make this module
 ## need precondition validation for region
 #module "super_intra_vpc_access" {
 #source = ""
