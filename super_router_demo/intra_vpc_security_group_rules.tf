279f972da8ec10326b071b3a3075b52799d66fab
Jude Quintana <jude.quintana@gmail.com>
2023-01-25 21:53:12 -0700

    updates

--- super_router_demo/intra_vpc_security_group_rules.tf
+++ super_router_demo/intra_vpc_security_group_rules.tf
@@ -3,6 +3,10 @@
 # This will create a sg rule for each vpc allowing inbound-only ports from
 # all other vpc networks (excluding itself)
 # Basically allowing ssh and ping communication across all VPCs.
+#output "vpcs_usw2" {
+#value = merge(module.vpcs_usw2, module.vpcs_usw2_another)
+#}
+
 locals {
   intra_vpc_security_group_rules = [
     {
@@ -18,8 +22,14 @@ locals {
       to_port   = 0
     }
   ]
-}
 
+  # better to do this to gaurantee map key uniqueness instead of merge(module.vpcs_usw2, module.vpcs_usw2_another) for each vpcs in same region which might overwrite keys
+  all_vpcs_usw2            = concat([for this in module.vpcs_usw2 : this], [for this in module.vpcs_usw2_another : this])
+  all_vpc_name_to_vpc_usw2 = { for this in local.all_vpcs_usw2 : this.name => this }
+
+  all_vpcs_use1            = concat([for this in module.vpcs_use1 : this], [for this in module.vpcs_use1_another : this])
+  all_vpc_name_to_vpc_use1 = { for this in local.all_vpcs_use1 : this.name => this }
+}
 
 module "intra_vpc_security_group_rules_usw2" {
   source = "git@github.com:JudeQuintana/terraform-modules.git//networking/intra_vpc_security_group_rule_for_tiered_vpc_ng?ref=moar-better"
@@ -35,7 +45,7 @@ module "intra_vpc_security_group_rules_usw2" {
   region_az_labels = var.region_az_labels
   intra_vpc_security_group_rule = {
     rule = each.value
-    vpcs = merge(module.vpcs_usw2, module.vpcs_usw2_another)
+    vpcs = local.all_vpc_name_to_vpc_usw2
   }
 }
 
@@ -53,13 +63,10 @@ module "intra_vpc_security_group_rules_use1" {
   region_az_labels = var.region_az_labels
   intra_vpc_security_group_rule = {
     rule = each.value
-    vpcs = merge(module.vpcs_use1, module.vpcs_use1_another)
+    #vpcs = merge(module.vpcs_use1, module.vpcs_use1_another)
+    vpcs = local.all_vpc_name_to_vpc_use1
   }
 }
-
-#output "intra_vpc_rules" {
-#value = merge(module.intra_vpc_security_group_rules_usw2, module.intra_vpc_security_group_rules_use1)
-#}
 
 module "super_intra_vpc_security_group_rules_usw2_to_use1" {
   source = "git@github.com:JudeQuintana/terraform-modules.git//networking/super_intra_vpc_security_group_rules?ref=moar-better"
@@ -81,6 +88,6 @@ module "super_intra_vpc_security_group_rules_usw2_to_use1" {
   }
 }
 
-#output "super_intra_vpc_rules" {
-#value = module.super_intra_vpc_security_group_rules_usw2_to_use1
-#}
+output "super_intra_vpc_rules" {
+  value = module.super_intra_vpc_security_group_rules_usw2_to_use1
+}
