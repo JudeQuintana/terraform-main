986223ea8187ed795e7c1ce8f61970223466d135
Jude Quintana <jude.quintana@gmail.com>
2023-01-31 19:49:02 -0700

    bump source versions

--- networking_trifecta_demo/vpcs.tf
+++ networking_trifecta_demo/vpcs.tf
@@ -58,7 +58,7 @@ locals {
 }
 
 module "vpcs" {
-  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/tiered_vpc_ng?ref=v1.4.6"
+  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/tiered_vpc_ng?ref=v1.4.7"
 
   for_each = { for t in local.tiered_vpcs : t.name => t }
 
--- super_router_demo/security_group_rules.tf
+++ super_router_demo/security_group_rules.tf
@@ -19,7 +19,7 @@ locals {
 }
 
 module "intra_vpc_security_group_rules_usw2" {
-  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/intra_vpc_security_group_rule_for_tiered_vpc_ng?ref=v1.4.6"
+  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/intra_vpc_security_group_rule_for_tiered_vpc_ng?ref=v1.4.7"
 
   providers = {
     aws = aws.usw2
@@ -36,7 +36,7 @@ module "intra_vpc_security_group_rules_usw2" {
 }
 
 module "intra_vpc_security_group_rules_use1" {
-  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/intra_vpc_security_group_rule_for_tiered_vpc_ng?ref=v1.4.6"
+  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/intra_vpc_security_group_rule_for_tiered_vpc_ng?ref=v1.4.7"
 
   providers = {
     aws = aws.use1
@@ -54,7 +54,7 @@ module "intra_vpc_security_group_rules_use1" {
 
 # allowing ssh and ping communication across regions
 module "super_intra_vpc_security_group_rules_usw2_to_use1" {
-  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/super_intra_vpc_security_group_rules?ref=v1.4.6"
+  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/super_intra_vpc_security_group_rules?ref=v1.4.7"
 
   providers = {
     aws.local = aws.usw2
