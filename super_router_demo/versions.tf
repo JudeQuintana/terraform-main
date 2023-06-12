f3212e36c50563e13cb69db7d55eff2067272102
Jude Quintana <jude.quintana@gmail.com>
2023-06-12 16:55:32 -0600

    upgrade tf 1.5.0 and aws provder latest 4.x

--- super_router_demo/versions.tf
+++ super_router_demo/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~>1.3" # allows 1.3, 1.4, 1.x
+  required_version = "~> 1.3" # allows 1.3, 1.4, 1.x.x
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>4.58"
+      version = "~> 4.67"
     }
   }
 }
