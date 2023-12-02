b0c3d868af29f37dad96e908bc863d09c6446a27
Jude Quintana <jude.quintana@gmail.com>
2023-12-02 12:28:53 -0700

    wip

--- full_mesh_trio_demo/security_group_rules.tf
+++ full_mesh_trio_demo/security_group_rules.tf
@@ -1,2 +1,95 @@
-# TODO:
-# - Create module that creates SG rules across 3 regions by consuming 3 intra_vpc_security_group_rules modules.
+locals {
+  intra_vpc_security_group_rules = [
+    {
+      label     = "ssh"
+      protocol  = "tcp"
+      from_port = 22
+      to_port   = 22
+    },
+    {
+      label     = "ping"
+      protocol  = "icmp"
+      from_port = 8
+      to_port   = 0
+    }
+  ]
+}
+
+module "intra_vpc_security_group_rules_use1" {
+  source  = "JudeQuintana/intra-vpc-security-group-rule/aws"
+  version = "1.0.0"
+
+  providers = {
+    aws = aws.use1
+  }
+
+  for_each = { for r in local.intra_vpc_security_group_rules : r.label => r }
+
+  env_prefix       = var.env_prefix
+  region_az_labels = var.region_az_labels
+  intra_vpc_security_group_rule = {
+    rule = each.value
+    vpcs = module.vpcs_use1
+  }
+}
+
+module "intra_vpc_security_group_rules_use2" {
+  source  = "JudeQuintana/intra-vpc-security-group-rule/aws"
+  version = "1.0.0"
+
+  providers = {
+    aws = aws.use2
+  }
+
+  for_each = { for r in local.intra_vpc_security_group_rules : r.label => r }
+
+  env_prefix       = var.env_prefix
+  region_az_labels = var.region_az_labels
+  intra_vpc_security_group_rule = {
+    rule = each.value
+    vpcs = module.vpcs_use2
+  }
+}
+
+module "intra_vpc_security_group_rules_usw2" {
+  source  = "JudeQuintana/intra-vpc-security-group-rule/aws"
+  version = "1.0.0"
+
+  providers = {
+    aws = aws.usw2
+  }
+
+  for_each = { for r in local.intra_vpc_security_group_rules : r.label => r }
+
+  env_prefix       = var.env_prefix
+  region_az_labels = var.region_az_labels
+  intra_vpc_security_group_rule = {
+    rule = each.value
+    vpcs = module.vpcs_usw2
+  }
+}
+
+module "full_mesh_intra_vpc_security_groups_rules" {
+  source = "/Users/jude/projects/terraform-modules/networking/full_mesh_intra_vpc_security_group_rules"
+
+  providers = {
+    aws.one   = aws.use1
+    aws.two   = aws.use2
+    aws.three = aws.usw2
+  }
+
+  env_prefix       = var.env_prefix
+  region_az_labels = var.region_az_labels
+  full_mesh_intra_vpc_security_group_rules = {
+    one = {
+      intra_vpc_security_group_rules = module.intra_vpc_security_group_rules_use1
+    }
+    two = {
+      intra_vpc_security_group_rules = module.intra_vpc_security_group_rules_use2
+    }
+    three = {
+      intra_vpc_security_group_rules = module.intra_vpc_security_group_rules_usw2
+    }
+  }
+}
+
