4a313bef6111427b2dc9a1a68b3dd016ca37c6e4
Jude Quintana <jude.quintana@gmail.com>
2024-09-21 21:31:20 -0600

    vpc peering

--- dual_stack_full_mesh_trio_demo/vpc_peering.tf
+++ dual_stack_full_mesh_trio_demo/vpc_peering.tf
@@ -1,46 +1,48 @@
 # cross region peering, only route specific subnets across peering connection
-#module "vpc_peering_deluxe_use1_general2_to_use2_cicd1" {
-#source  = "JudeQuintana/vpc-peering-deluxe/aws"
-#version = "1.0.0"
+module "vpc_peering_deluxe_use1_general3_to_use2_app1" {
+  #source  = "JudeQuintana/vpc-peering-deluxe/aws"
+  #version = "1.0.0"
+  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/vpc_peering_deluxe?ref=dual-stack-full-mesh-trio"
 
-#providers = {
-#aws.local = aws.use1
-#aws.peer  = aws.use2
-#}
+  providers = {
+    aws.local = aws.use1
+    aws.peer  = aws.use2
+  }
 
-#env_prefix = var.env_prefix
-#vpc_peering_deluxe = {
-#local = {
-#vpc = lookup(module.vpcs_use1, "general2")
-## use1 public random1
-#only_route_subnet_cidrs = ["192.168.13.0/28"]
-#}
-#peer = {
-#vpc = lookup(module.vpcs_use2, "cicd1")
-## use2 private jenkins1
-#only_route_subnet_cidrs = ["172.16.1.0/24"]
-#}
-#}
-#}
+  env_prefix = var.env_prefix
+  vpc_peering_deluxe = {
+    local = {
+      vpc                          = lookup(module.vpcs_use1, "general3")
+      only_route_subnet_cidrs      = ["192.168.65.0/24"]
+      only_route_ipv6_subnet_cidrs = ["2600:1f28:3d:c400::/64"]
+    }
+    peer = {
+      vpc                          = lookup(module.vpcs_use2, "app1")
+      only_route_subnet_cidrs      = ["172.16.128.0/24"]
+      only_route_ipv6_subnet_cidrs = ["2600:1f26:21:c004::/64"]
+    }
+  }
+}
 
 ## inter region vpc peering, route all subnets across peering connection
-#module "vpc_peering_deluxe_usw2_app1_to_usw2_general1" {
-#source  = "JudeQuintana/vpc-peering-deluxe/aws"
-#version = "1.0.0"
+module "vpc_peering_deluxe_usw2_app2_to_usw2_general2" {
+  #source  = "JudeQuintana/vpc-peering-deluxe/aws"
+  #version = "1.0.0"
+  source = "git@github.com:JudeQuintana/terraform-modules.git//networking/vpc_peering_deluxe?ref=dual-stack-full-mesh-trio"
 
-#providers = {
-#aws.local = aws.usw2
-#aws.peer  = aws.usw2
-#}
+  providers = {
+    aws.local = aws.usw2
+    aws.peer  = aws.usw2
+  }
 
-#env_prefix = var.env_prefix
-#vpc_peering_deluxe = {
-#local = {
-#vpc = lookup(module.vpcs_usw2, "app1")
-#}
-#peer = {
-#vpc = lookup(module.vpcs_usw2, "general1")
-#}
-#}
-#}
+  env_prefix = var.env_prefix
+  vpc_peering_deluxe = {
+    local = {
+      vpc = lookup(module.vpcs_usw2, "app2")
+    }
+    peer = {
+      vpc = lookup(module.vpcs_usw2, "general2")
+    }
+  }
+}
 
