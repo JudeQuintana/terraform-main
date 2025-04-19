2011cf209e2f09f6fe4555270795747a7def729f
Jude Quintana <jude.quintana@gmail.com>
2025-04-19 08:14:58 -0600

    fix

--- full_mesh_trio_demo/vpc_endpoint.tf
+++ full_mesh_trio_demo/vpc_endpoint.tf
@@ -1,7 +1,7 @@
 locals {
   vpc_endpoint_service_name_fmt = "com.amazonaws.%s.s3"
   vpc_endpoint_type             = "Gateway"
-  vpcs_use1_all                 = { for this in merge(module.vpcs_use1, module.vpc_another_use1) : this.name => this }
+  vpcs_use1                     = { for this in module.vpcs_use1 : this.name => this }
 }
 
 # at scale we're saving money right here
@@ -10,7 +10,7 @@ resource "aws_vpc_endpoint" "s3" {
     aws = aws.use1
   }
 
-  for_each = local.vpcs_use1_all
+  for_each = local.vpcs_use1
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -19,7 +19,25 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 locals {
-  vpcs_usw2_all = { for this in merge(module.vpcs_usw2, module.vpc_another_usw2) : this.name => this }
+  vpcs_use2 = { for this in module.vpcs_use2 : this.name => this }
+}
+
+# at scale we're saving money right here
+resource "aws_vpc_endpoint" "s3" {
+  providers = {
+    aws = aws.use2
+  }
+
+  for_each = local.vpcs_use2
+
+  vpc_id            = each.value.id
+  service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
+  vpc_endpoint_type = local.vpc_endpoint_type
+  route_table_ids   = each.value.private_route_table_ids
+}
+
+locals {
+  vpcs_usw2 = { for this in module.vpcs_usw2 : this.name => this }
 }
 
 # at scale we're saving money right here
@@ -28,7 +46,7 @@ resource "aws_vpc_endpoint" "s3" {
     aws = aws.usw2
   }
 
-  for_each = local.vpcs_usw2_all
+  for_each = local.vpcs_usw2
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
