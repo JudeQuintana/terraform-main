cb0d5d6bb274ed15e4332dd6d3a4ff3a0147e84f
Jude Quintana <jude.quintana@gmail.com>
2025-04-19 08:07:52 -0600

    update

--- super_router_demo/vpc_endpoint.tf
+++ super_router_demo/vpc_endpoint.tf
@@ -1,6 +1,9 @@
 locals {
-  vpcs_use1_all = { for this in merge(module.vpcs_use1, module.vpc_another_use1) : this.name => this }
+  vpc_endpoint_service_name_fmt = "com.amazonaws.%s.s3"
+  vpc_endpoint_type             = "Gateway"
+  vpcs_use1_all                 = { for this in merge(module.vpcs_use1, module.vpc_another_use1) : this.name => this }
 }
+
 # at scale we're saving money right here
 resource "aws_vpc_endpoint" "s3" {
   providers = {
@@ -10,14 +13,15 @@ resource "aws_vpc_endpoint" "s3" {
   for_each = local.vpcs_use1_all
 
   vpc_id            = each.value.id
-  service_name      = format("com.amazonaws.%s.s3", each.value.region)
-  vpc_endpoint_type = "Gateway"
+  service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
+  vpc_endpoint_type = local.vpc_endpoint_type
   route_table_ids   = each.value.private_route_table_ids
 }
 
 locals {
   vpcs_usw2_all = { for this in merge(module.vpcs_usw2, module.vpc_another_usw2) : this.name => this }
 }
+
 # at scale we're saving money right here
 resource "aws_vpc_endpoint" "s3" {
   providers = {
@@ -27,8 +31,8 @@ resource "aws_vpc_endpoint" "s3" {
   for_each = local.vpcs_usw2_all
 
   vpc_id            = each.value.id
-  service_name      = format("com.amazonaws.%s.s3", each.value.region)
-  vpc_endpoint_type = "Gateway"
+  service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
+  vpc_endpoint_type = local.vpc_endpoint_type
   route_table_ids   = each.value.private_route_table_ids
 }
 
