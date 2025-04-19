47c56dbcbec3bc49073c3e2708351ca96e874445
Jude Quintana <jude.quintana@gmail.com>
2025-04-19 09:05:01 -0600

    updates

---
+++ centralized_egress_dual_stack_full_mesh_trio_demo/vpc_endpoints.tf
@@ -0,0 +1,45 @@
+# at scale we're saving money right here
+locals {
+  vpc_endpoint_service_name_fmt = "com.amazonaws.%s.s3"
+  vpc_endpoint_type             = "Gateway"
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  providers = {
+    aws = aws.use1
+  }
+
+  for_each = module.vpcs_use1
+
+  vpc_id            = each.value.id
+  service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
+  vpc_endpoint_type = local.vpc_endpoint_type
+  route_table_ids   = each.value.private_route_table_ids
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  providers = {
+    aws = aws.use2
+  }
+
+  for_each = module.vpcs_use2
+
+  vpc_id            = each.value.id
+  service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
+  vpc_endpoint_type = local.vpc_endpoint_type
+  route_table_ids   = each.value.private_route_table_ids
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  providers = {
+    aws = aws.usw2
+  }
+
+  for_each = module.vpcs_usw2
+
+  vpc_id            = each.value.id
+  service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
+  vpc_endpoint_type = local.vpc_endpoint_type
+  route_table_ids   = each.value.private_route_table_ids
+}
+
