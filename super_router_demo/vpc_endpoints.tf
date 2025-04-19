2ac91d25d947a67d12f4451c1d64324cbb3c87fe
Jude Quintana <jude.quintana@gmail.com>
2025-04-19 09:27:42 -0600

    updates

--- super_router_demo/vpc_endpoints.tf
+++ super_router_demo/vpc_endpoints.tf
@@ -2,7 +2,6 @@
 locals {
   vpc_endpoint_service_name_fmt = "com.amazonaws.%s.s3"
   vpc_endpoint_type             = "Gateway"
-  vpcs_use1_all                 = merge(module.vpcs_use1, module.vpc_another_use1)
 }
 
 resource "aws_vpc_endpoint" "s3_use1" {
@@ -10,7 +9,7 @@ resource "aws_vpc_endpoint" "s3_use1" {
     aws = aws.use1
   }
 
-  for_each = local.vpcs_use1_all
+  for_each = merge(module.vpcs_use1, module.vpc_another_use1)
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -18,16 +17,12 @@ resource "aws_vpc_endpoint" "s3_use1" {
   route_table_ids   = each.value.private_route_table_ids
 }
 
-locals {
-  vpcs_usw2_all = merge(module.vpcs_usw2, module.vpc_another_usw2)
-}
-
 resource "aws_vpc_endpoint" "s3_usw2" {
   providers = {
     aws = aws.usw2
   }
 
-  for_each = local.vpcs_usw2_all
+  for_each = merge(module.vpcs_usw2, module.vpc_another_usw2)
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
