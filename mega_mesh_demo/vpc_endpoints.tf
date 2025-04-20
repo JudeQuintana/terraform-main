14d60165b9225eeb19278959f8d6bebc4f75ad8f
Jude Quintana <jude.quintana@gmail.com>
2025-04-20 12:38:50 -0600

    s3 gateway vpc filter

--- mega_mesh_demo/vpc_endpoints.tf
+++ mega_mesh_demo/vpc_endpoints.tf
@@ -2,12 +2,24 @@
 locals {
   vpc_endpoint_service_name_fmt = "com.amazonaws.%s.s3"
   vpc_endpoint_type             = "Gateway"
+
+
+  vpcs_apne1_with_private_route_table_ids = { for this in module.vpcs_apne1 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_apse1_with_private_route_table_ids = { for this in module.vpcs_apse1 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_cac1_with_private_route_table_ids  = { for this in module.vpcs_cac1 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_euc1_with_private_route_table_ids  = { for this in module.vpcs_euc1 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_euw1_with_private_route_table_ids  = { for this in module.vpcs_euw1 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_sae1_with_private_route_table_ids  = { for this in module.vpcs_usw2 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_use1_with_private_route_table_ids  = { for this in module.vpcs_use1 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_use2_with_private_route_table_ids  = { for this in module.vpcs_use2 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_usw1_with_private_route_table_ids  = { for this in module.vpcs_usw1 : this.name => this if length(this.private_route_table_ids) > 0 }
+  vpcs_usw2_with_private_route_table_ids  = { for this in module.vpcs_usw2 : this.name => this if length(this.private_route_table_ids) > 0 }
 }
 
 resource "aws_vpc_endpoint" "s3_apne1" {
   provider = aws.apne1
 
-  for_each = module.vpcs_apne1
+  for_each = local.vpcs_apne1_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -18,7 +30,7 @@ resource "aws_vpc_endpoint" "s3_apne1" {
 resource "aws_vpc_endpoint" "s3_apse1" {
   provider = aws.apse1
 
-  for_each = module.vpcs_apse1
+  for_each = local.vpcs_apse1_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -29,7 +41,7 @@ resource "aws_vpc_endpoint" "s3_apse1" {
 resource "aws_vpc_endpoint" "s3_cac1" {
   provider = aws.cac1
 
-  for_each = module.vpcs_cac1
+  for_each = local.vpcs_cac1_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -40,7 +52,7 @@ resource "aws_vpc_endpoint" "s3_cac1" {
 resource "aws_vpc_endpoint" "s3_euc1" {
   provider = aws.euc1
 
-  for_each = module.vpcs_euc1
+  for_each = local.vpcs_euc1_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -51,7 +63,7 @@ resource "aws_vpc_endpoint" "s3_euc1" {
 resource "aws_vpc_endpoint" "s3_euw1" {
   provider = aws.euw1
 
-  for_each = module.vpcs_euw1
+  for_each = local.vpcs_euw1_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -62,7 +74,7 @@ resource "aws_vpc_endpoint" "s3_euw1" {
 resource "aws_vpc_endpoint" "s3_sae1" {
   provider = aws.sae1
 
-  for_each = module.vpcs_sae1
+  for_each = local.vpcs_sae1_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -73,7 +85,7 @@ resource "aws_vpc_endpoint" "s3_sae1" {
 resource "aws_vpc_endpoint" "s3_use1" {
   provider = aws.use1
 
-  for_each = module.vpcs_use1
+  for_each = local.vpcs_use1_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -84,7 +96,7 @@ resource "aws_vpc_endpoint" "s3_use1" {
 resource "aws_vpc_endpoint" "s3_use2" {
   provider = aws.use2
 
-  for_each = module.vpcs_use2
+  for_each = local.vpcs_use2_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -95,7 +107,7 @@ resource "aws_vpc_endpoint" "s3_use2" {
 resource "aws_vpc_endpoint" "s3_usw1" {
   provider = aws.usw1
 
-  for_each = module.vpcs_usw1
+  for_each = local.vpcs_usw1_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
@@ -106,7 +118,7 @@ resource "aws_vpc_endpoint" "s3_usw1" {
 resource "aws_vpc_endpoint" "s3_usw2" {
   provider = aws.usw2
 
-  for_each = module.vpcs_usw2
+  for_each = local.vpcs_usw2_with_private_route_table_ids
 
   vpc_id            = each.value.id
   service_name      = format(local.vpc_endpoint_service_name_fmt, each.value.region)
